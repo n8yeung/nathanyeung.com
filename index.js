0c18071fdf04c395e8d949db301e58f976dbec5e
Nathan Yeung <nathan@nathanyeung.com>
2021-07-19 14:11:12 -0700

    M-0001: Updated routes

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Home
-app.get('/', (req, res) => res.send('Home Page Route'));
+app.get("/", (req, res) => res.send("Home Page Route"));
 
 // About
-// app.get('/about', (req, res) => res.send('About Page Route'));
+app.get("/about", (req, res) => res.send("About Page Route"));
 
 // POrtfolio
-// app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
-
-// app.get('/contact', (req, res) => res.send('Contact Page Route'));
+app.get("/portfolio", (req, res) => res.send("Portfolio Page Route"));
+// Contact
+app.get("/contact", (req, res) => res.send("Contact Page Route"));
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
+app.listen(port, () =>
+  console.log(`Server running on ${port}, http://localhost:${port}`)
+);
