5ddab1528cc893633022974ed19015b0847c6bf1
Nathan Yeung <nathan@nathanyeung.com>
2021-07-21 08:56:04 -0700

    M-0001: Adding disclaimer

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 
 // Home
-app.get("/", (req, res) => res.send("<h1>Home Page Route</h1>"));
-
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/static/index.html");
+// });
 // About
 app.get("/about", (req, res) => res.send("About Page Route"));
 
